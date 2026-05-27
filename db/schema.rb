b14f588ffc1224d3d80276f556d590f03d6797d4
agirmai <agirmai737@gmail.com>
2026-05-26 20:05:54 -0500

    fixed some issues regarding wsl versus windows

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_05_19_180933) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_26_222900) do
   create_table "todos", force: :cascade do |t|
-    t.string "description"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "description"
     t.datetime "due_date"
+    t.boolean "high_priority", default: false, null: false
+    t.string "owner_token"
+    t.datetime "updated_at", null: false
+    t.index ["owner_token"], name: "index_todos_on_owner_token"
   end
 end
--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -45,4 +45,14 @@ class TodosControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to todos_url
   end
+
+  test "should toggle priority via turbo stream" do
+    assert_not @todo.high_priority
+
+    patch toggle_priority_todo_url(@todo), as: :turbo_stream
+
+    assert_response :success
+    assert_equal "text/vnd.turbo-stream.html", response.media_type
+    assert @todo.reload.high_priority
+  end
 end
